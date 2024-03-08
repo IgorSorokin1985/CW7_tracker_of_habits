b1bb436f1aaae903ebddb9b63a2c0fc1151c55ff
sorokin <isorokin1985@gmail.com>
2024-03-08 18:20:29 +0300

    tests

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from habits.models import Habit
+from users.models import User
 
-# Create your tests here.
+
+class CourseAPITest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            id=10,
+            name="testuser",
+            email="testuser@test.com",
+            telegram="@testtelegram",
+            password='12345'
+        )
+        self.habit = Habit.objects.create(
+            id=100,
+            user=self.user,
+            place="home",
+            action="not drink alcohol",
+            is_nice_habit=False,
+            reward="15 DKK",
+            periodicity=1,
+            is_public=False,
+            time_for_habit=15,
+        )
+
+    def test_get_list_habits(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(
+            '/habit/')
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_create_habit(self):
+        data = {
+            "id": 101,
+            "place": "home",
+            "action": "drink water",
+            "is_nice_habit": False,
+            "reward": "10 DKK",
+            "periodicity": 1,
+            "is_public": False,
+            "time_for_habit": 15,
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            '/habit/create/',
+            data=data)
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_delete_habit(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(
+            '/habit/delete/100/')
+
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+    def test_get_list_public_habits(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(
+            '/habit/public/')
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_200_OK
+        )
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User
+from users.models import User
 
-# Create your tests here.
+
+class CourseAPITest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            id=11,
+            name="testuser1",
+            email="testuser1@test.com",
+            telegram="@testtelegram1",
+            password='12345'
+        )
+
+    def test_get_user(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(
+            '/user/11/')
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_create_user(self):
+        data = {
+            "name": "testuser2",
+            "email": "testuser2@test.com",
+            "telegram": "@testtelegram2",
+            "password": "12345"
+        }
+        #self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            '/user/create/',
+            data=data)
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_delete_user(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(
+            '/user/delete/11/')
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
