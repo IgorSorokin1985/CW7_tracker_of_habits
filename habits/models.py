6a94d12565d547e48b29d27e6b233df20569922d
sorokin <isorokin1985@gmail.com>
2024-03-08 13:04:08 +0300

    crate models user and habit

---
+++ habits/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, verbose_name='user')
+    place = models.CharField(max_length=50, verbose_name='Place')
+    action = models.CharField(max_length=50, verbose_name='Action')
+    is_nice_habit = models.BooleanField(**NULLABLE, verbose_name='Is nice habit?')
+    reward = models.CharField(max_length=50, **NULLABLE, verbose_name='Reward')
+    associated_habit = models.ForeignKey('self', **NULLABLE, verbose_name='Associated habit')
+    periodicity = models.PositiveIntegerField(verbose_name='Periodicity')
+    is_public = models.BooleanField(default=False, verbose_name='Is public')
+    time_for_habit = models.PositiveIntegerField(verbose_name='Time for habit')
+
+    def __str__(self):
+        return f'{self.action}'
+
+    class Meta:
+        verbose_name = 'Habit'
+        verbose_name_plural = 'Habits'
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    name = models.CharField(max_length=50, verbose_name='Name')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    telegram = models.CharField(max_length=50, verbose_name='Telegram')
+    avatar = models.ImageField(upload_to='users', **NULLABLE, verbose_name='Avatar')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
