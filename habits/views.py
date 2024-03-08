204233d1d24a81f1c45b54908dca3ad7e9f3aa84
sorokin <isorokin1985@gmail.com>
2024-03-08 17:30:04 +0300

    paginators

--- habits/views.py
+++ habits/views.py
@@ -4,6 +4,7 @@ from habits.models import Habit
 from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsOwner
 from habits.serializers import HabitSerializer, HabitsSerializer
+from habits.paginators import HabitsPagination
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 #from materials.utils import get_url_for_payment
@@ -23,7 +24,7 @@ class HabitListAPIView(generics.ListAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticated, IsOwner]
-    #pagination_class = MaterialsPagination
+    pagination_class = HabitsPagination
 
     def get_queryset(self):
         list_habits = super().get_queryset()
@@ -34,7 +35,7 @@ class HabitsPublicListAPIView(generics.ListAPIView):
     serializer_class = HabitsSerializer
     queryset = Habit.objects.all().filter(is_public=True)
     permission_classes = [IsAuthenticated]
-    #pagination_class = MaterialsPagination
+    pagination_class = HabitsPagination
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
