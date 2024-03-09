5d0c627ab768a95ef03fe3329e95edd9b41589c5
sorokin <isorokin1985@gmail.com>
2024-03-09 22:00:59 +0300

    add changing next_date

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,16 +1,21 @@
 from habits.utils import send_telegram_message
 from celery import shared_task
 from habits.models import Habit
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @shared_task
 def check_habits_for_action():
     habits = Habit.objects.all()
-    now = datetime.now().time()
+    now_time = datetime.now().time()
+    now_date = datetime.now().today()
     for habit in habits:
         if not habit.next_date:
-            print(habit.time)
-            print(now)
-            if habit.time < now:
+            if habit.time < now_time:
                 send_telegram_message(habit)
+                habit.next_date = now_date + timedelta(days=habit.periodicity)
+                habit.save()
+        elif habit.next_date <= now_date:
+            send_telegram_message(habit)
+            habit.next_date = now_date + timedelta(days=habit.periodicity)
+            habit.save()
